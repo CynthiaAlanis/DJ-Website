de3e2c1dd827d8002cdb9160b73c7d2607207dc0
cynthia <santoro.cynthia@yahoo.com>
2023-06-16 17:05:25 -0700

    delete

--- src/components/Video/Videos.js
+++ src/components/Video/Videos.js
@@ -2,14 +2,11 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ReactPlayer from 'react-player';
 import Particle from "../Particle";
-import halloweenV from "../../Assets/Projects/videos/halloween.mp4";
-import schoolV from "../../Assets/Projects/videos/school.mp4";
-import birthday from "../../Assets/Projects/videos/Birthday.mp4";
-import GradV from "../../Assets/Projects/videos/Grad.mp4";
-import GradV2 from "../../Assets/Projects/videos/grad2.mp4";
-import GradV3 from "../../Assets/Projects/videos/Grad3.mp4";
-import wedding1 from "../../Assets/Projects/videos/wedding.mp4";
-import fifteen from "../../Assets/Projects/videos/15th.mp4"
+
+
+
+
+
 function Videos() { 
 
   return(
@@ -27,7 +24,7 @@ function Videos() {
           <Col md={4} className="project-card">
 
           <ReactPlayer
-            url={wedding1}
+            url="https://i.imgur.com/PHHhJ61.mp4"
             title="Wedding"
             width="100%"
             height="100%"
@@ -35,42 +32,41 @@ function Videos() {
         />
           </Col>
 
-          <Col md={4} className="project-card">
-
+          {/* <Col md={4} className="project-card">
           <ReactPlayer
-            url={halloweenV}
+            url=""
             title="Halloween"
             width="100%"
             height="100%"
             controls
         />
-          </Col>
+          </Col> */}
 
           <Col md={4} className="project-card">
           <ReactPlayer
-            url={schoolV}
-            title="School Dance"
+            url="https://i.imgur.com/pLAA4By.mp4"
+            title="Wedding"
             width="100%"
             height="100%"
             controls
         />
 
-          </Col>
-
+        </Col>
+{/* 
           <Col md={4} className="project-card">
           <ReactPlayer
-            url={GradV}
+            url=""
             title="Graduation"
             width="100%"
             height="100%"
             controls
         />
-
-          </Col>
+          </Col>  */}
+          
 
           <Col md={4} className="project-card">
           <ReactPlayer
-            url={GradV2}
+            url="https://i.imgur.com/Be6hiGF.mp4"
             title="Graduation"
             width="100%"
             height="100%"
@@ -79,7 +75,7 @@ function Videos() {
         </Col>
           <Col md={4} className="project-card">
           <ReactPlayer
-            url={GradV3}
+            url="https://i.imgur.com/cTJPBZE.mp4"
             title="Graduation"
             width="100%"
             height="100%"
@@ -88,7 +84,7 @@ function Videos() {
           </Col>
           <Col md={4} className="project-card">
           <ReactPlayer
-            url={GradV3}
+            url="https://i.imgur.com/umuYGtT.mp4"
             title="Graduation"
             width="100%"
             height="100%"
@@ -98,7 +94,7 @@ function Videos() {
 
           <Col md={4} className="project-card">
           <ReactPlayer
-            url={birthday}
+            url='https://youtu.be/i1hBlH6ai1w'
             width="100%"
             height="100%"
             controls
@@ -106,7 +102,15 @@ function Videos() {
           </Col>
           <Col md={4} className="project-card">
           <ReactPlayer
-            url={fifteen}
+            url="https://i.imgur.com/rGABoFs.mp4"
+            width="100%"
+            height="100%"
+            controls
+        />
+          </Col>
+          <Col md={4} className="project-card">
+          <ReactPlayer
+            url="https://i.imgur.com/N8Dtnyd.mp4"
             width="100%"
             height="100%"
             controls
