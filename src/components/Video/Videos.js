9673d46ebcfe24112328e266252ee608abc8cd99
cynthia <santoro.cynthia@yahoo.com>
2023-06-23 18:56:11 -0700

    update

--- src/components/Video/Videos.js
+++ src/components/Video/Videos.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ReactPlayer from 'react-player';
+
 import Particle from "../Particle";
 
 
-
-
-
-function Videos() { 
+const Player =() =>{ 
 
   return(
     
@@ -23,117 +21,112 @@ function Videos() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
  
-          <ReactPlayer
-            url={"https://i.imgur.com/PHHhJ61.mp4"}
+            <ReactPlayer
+            url="https://i.imgur.com/PHHhJ61.mp4"
             title="Wedding"
             width="100%"
-            height="100%"
-            playIcon={<button>Play</button>}
+            height="100%"           
             controls={true}
             type="video/mp4"
-        />
+            />
           </Col>
 
           <Col md={4} className="project-card">
-          <ReactPlayer
+            <ReactPlayer
             url="https://i.imgur.com/LvrWIvX.mp4"
             title="Graduation"
             width="100%"
-            height="100%"
+            height="100%"  
             controls={true}
             type="video/mp4"
-        />
+            />
           </Col>
 
           <Col md={4} className="project-card">
-          <ReactPlayer
+            <ReactPlayer
             url="https://i.imgur.com/xoKuhDz.mp4"
             title="Graduation"
             width="100%"
-            height="100%"
+            height="100%"   
             controls={true}
             type="video/mp4"
-        />
-       
-        </Col>
+            />
+          </Col>
 
           <Col md={4} className="project-card">
-          <ReactPlayer
+            <ReactPlayer
             url="https://i.imgur.com/GsfY5qc.mp4"
             title="Graduation"
             width="100%"
-            height="100%"
+            height="100%"     
             controls={true}
             type="video/mp4"
-        />
+            />
           </Col> 
-          
 
           <Col md={4} className="project-card">
-          <ReactPlayer
+            <ReactPlayer
             url="https://i.imgur.com/Be6hiGF.mp4"
             title="Graduation"
             width="100%"
-            height="100%"
+            height="100%"        
             controls={true}
             type="video/mp4"
-        />
-         
-        </Col>
+            />
+          </Col>
+
           <Col md={4} className="project-card">
-          <ReactPlayer
+            <ReactPlayer
             url="https://i.imgur.com/cTJPBZE.mp4"
             title="Graduation"
             width="100%"
             height="100%"
             controls={true}
             type="video/mp4"
-        />
-         
-          </Col>
-          <Col md={4} className="project-card">
-          <ReactPlayer
-            url="https://i.imgur.com/umuYGtT.mp4"
-            title="Graduation"
-            width="100%"
-            height="100%"
-            controls={true}
-            type="video/mp4"
-        />
-         
+            />
           </Col>
 
           <Col md={4} className="project-card">
-          <ReactPlayer
+            <ReactPlayer    
+            url="https://i.imgur.com/umuYGtT.mp4"
+            title="Graduation"
+            width="100%"
+            height="100%"        
+            controls={true}
+            type="video/mp4"
+            />
+
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ReactPlayer 
             url='https://i.imgur.com/zHh9vvv.mp4'
             title="Wedding"
             width="100%"
-            height="100%"
+            height="100%"   
             controls={true}
             type="video/mp4"
-        />
-         
+            />
           </Col>
+
           <Col md={4} className="project-card">
-          <ReactPlayer
-          
+            <ReactPlayer 
             url="https://i.imgur.com/rGABoFs.mp4"
             width="100%"
             height="100%"
             controls={true}
             type="video/mp4"
-        />
-        
+            />
           </Col>
+
           <Col md={4} className="project-card">
-          <ReactPlayer
+            <ReactPlayer 
             url="https://i.imgur.com/N8Dtnyd.mp4"
             width="100%"
             height="100%"
             controls={true}
             type="video/mp4"
-        />
-        
+            />
           </Col>
         </Row>
       </Container>
@@ -141,5 +134,5 @@ function Videos() {
   );
 }
 
-export default Videos;
+export default Player;
 
